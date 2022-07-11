482239b6d74d3fd8cdc011edc240cc21eb500ee4
Antonio Stoilkov <hello@astoilkov.com>
2022-07-11 15:29:48 +0300

    ✏️ improve comments in jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -1,15 +1,11 @@
 import type { Config } from '@jest/types'
 
 const config: Config.InitialOptions = {
+    // adds support for tests written in TypeScript
     preset: 'ts-jest',
-    testEnvironment: 'jsdom',
 
-    // 🐛 fix "having test.js and test.ts doesn't work"
-    // what about: ts-node CLI option:
-    // --prefer-ts-exts Re-order file extensions so that TypeScript imports are preferred (TS_NODE_PREFER_TS_EXTS, default: false)
-    moduleNameMapper: {
-        '^(\\..*)$': ['$1/index.ts', '$1.ts', '$1'],
-    },
+    // adds jsdom (https://github.com/jsdom/jsdom) APIs to tests
+    testEnvironment: 'jsdom',
 
     // run tests on root and in /test folder with .ts and .tsx extensions
     testMatch: ['<rootDir>/test.ts?(x)', '<rootDir>/test/**.ts?(x)'],
@@ -23,8 +19,18 @@ const config: Config.InitialOptions = {
     },
     extensionsToTreatAsEsm: ['.ts', '.tsx'],
 
-    // https://jestjs.io/docs/configuration#restoremocks-boolean
+    // restores original implementation (after each test is finished) when calling `jest.spyOn()`
+    // inside of a test:
+    // - https://jestjs.io/docs/configuration#restoremocks-boolean
+    // - https://jestjs.io/docs/mock-function-api#mockfnmockrestore
     restoreMocks: true,
+
+    // 🐛 fix "having test.js and test.ts runs the test.js file"
+    // what about: ts-node CLI option:
+    // --prefer-ts-exts Re-order file extensions so that TypeScript imports are preferred (TS_NODE_PREFER_TS_EXTS, default: false)
+    moduleNameMapper: {
+        '^(\\..*)$': ['$1/index.ts', '$1.ts', '$1'],
+    },
 }
 
 export default config
