bcbb1c7036d6ea818dbf29a68169e8113173843e
Alisson <thelinuxlich@gmail.com>
2024-01-24 11:16:47 -0300

    better types for goTryRaw, so we can override the error on the generic param

--- index.ts
+++ index.ts
@@ -1,94 +1,102 @@
-import pIsPromise from "p-is-promise";
-type ResultTuple<T> = readonly [undefined, T] | readonly [string, undefined];
-type RawResultTuple<T> = readonly [undefined, T] | readonly [unknown, undefined];
+import pIsPromise from 'p-is-promise'
+type ResultTuple<T> = readonly [undefined, T] | readonly [string, undefined]
+type RawResultTuple<T, E = unknown> =
+	| readonly [undefined, T]
+	| readonly [E, undefined]
 
 type ErrorWithMessage = {
-	message: string;
-};
+	message: string
+}
 
 function isErrorWithMessage(error: unknown): error is ErrorWithMessage {
 	return (
-		typeof error === "object" &&
+		typeof error === 'object' &&
 		error !== null &&
-		"message" in error &&
-		typeof (error as Record<string, unknown>).message === "string"
-	);
+		'message' in error &&
+		typeof (error as Record<string, unknown>).message === 'string'
+	)
 }
 
 function toErrorWithMessage(maybeError: unknown): ErrorWithMessage {
 	if (isErrorWithMessage(maybeError)) {
-		return maybeError;
+		return maybeError
 	}
 
 	try {
-		return new Error(JSON.stringify(maybeError));
+		return new Error(JSON.stringify(maybeError))
 	} catch {
 		// fallback in case there's an error stringifying the maybeError
-		// like with circular references for example.
-		return new Error(String(maybeError));
+		//  with circular references for example.
+		return new Error(String(maybeError))
 	}
 }
 
 function getErrorMessage(error: unknown): string {
-	return toErrorWithMessage(error).message;
+	return toErrorWithMessage(error).message
 }
 
-function isPromise<T>(p: T | PromiseLike<T>): p is PromiseLike<T> {
-	return pIsPromise(p);
+function isPromise<T>(p: T | Promise<T>): p is Promise<T> {
+	return pIsPromise(p)
 }
 
-function goTry<T>(value: PromiseLike<T>): PromiseLike<ResultTuple<T>>;
-function goTry<T>(value: () => T): ResultTuple<T>;
-function goTry<T>(value: (() => T) | PromiseLike<T>): ResultTuple<T> | PromiseLike<ResultTuple<T>> {
-	let unwrappedValue;
+function goTry<T>(value: Promise<T>): Promise<ResultTuple<T>>
+function goTry<T>(value: () => T): ResultTuple<T>
+function goTry<T>(
+	value: (() => T) | Promise<T>,
+): ResultTuple<T> | Promise<ResultTuple<T>> {
+	let unwrappedValue
 	try {
-		unwrappedValue = typeof value === "function" ? value() : value;
+		unwrappedValue = typeof value === 'function' ? value() : value
 		if (isPromise(unwrappedValue)) {
 			return Promise.resolve(unwrappedValue)
 				.then((value) => [undefined, value] as const)
-				.catch((err) => [getErrorMessage(err), undefined] as const);
+				.catch((err) => [getErrorMessage(err), undefined] as const)
 		}
-		return [undefined, unwrappedValue] as const;
+		return [undefined, unwrappedValue] as const
 	} catch (err) {
-		return [getErrorMessage(err), undefined] as const;
+		return [getErrorMessage(err), undefined] as const
 	}
 }
 
-function goTryRaw<T>(value: PromiseLike<T>): PromiseLike<RawResultTuple<T>>;
-function goTryRaw<T>(value: () => T): RawResultTuple<T>;
-function goTryRaw<T>(value: (() => T) | PromiseLike<T>): RawResultTuple<T> | PromiseLike<RawResultTuple<T>> {
-	let unwrappedValue;
+function goTryRaw<T, E = unknown>(
+	value: Promise<T>,
+): Promise<RawResultTuple<T, E>>
+function goTryRaw<T, E = unknown>(value: () => T): RawResultTuple<T, E>
+function goTryRaw<T, E = unknown>(
+	value: Promise<T> | (() => T),
+): RawResultTuple<T, E> | Promise<RawResultTuple<T, E>> {
+	let unwrappedValue
 	try {
-		unwrappedValue = typeof value === "function" ? value() : value;
+		unwrappedValue = typeof value === 'function' ? value() : value
 		if (isPromise(unwrappedValue)) {
 			return Promise.resolve(unwrappedValue)
 				.then((value) => [undefined, value] as const)
-				.catch((err: unknown) => [err, undefined] as const);
+				.catch((err) => [err, undefined] as const)
 		}
-		return [undefined, unwrappedValue] as const;
-	} catch (err: unknown) {
-		return [err, undefined] as const;
+		return [undefined, unwrappedValue] as const
+	} catch (err) {
+		return [err, undefined] as const as RawResultTuple<T, E>
 	}
 }
 
-async function goExpect<T>(value: (() => T) | PromiseLike<T>, error?: (err: string) => string): Promise<T> {
-	if (error === undefined) {
-		error = (e: string): string => e;
-	}
+async function goExpect<T>(
+	value: (() => T) | Promise<T>,
+	error?: (err: string) => string,
+): Promise<T> {
+	const _error = error ?? ((e: string): string => e)
 	try {
-		const unwrappedValue = typeof value === "function" ? value() : value;
+		const unwrappedValue = typeof value === 'function' ? value() : value
 		if (isPromise(unwrappedValue)) {
-			const [err, res] = await goTry(unwrappedValue);
+			const [err, res] = await goTry(unwrappedValue)
 			if (err !== undefined) {
-				throw new Error(err);
-			} else {
-				return res;
+				throw new Error(err)
 			}
+			return res
 		}
-		return unwrappedValue;
+		return unwrappedValue
 	} catch (err) {
-		throw new Error(error(getErrorMessage(err)));
+		throw new Error(_error(getErrorMessage(err)))
 	}
 }
 
-export { goTry, goExpect, goTryRaw };
+export { goTry, goExpect, goTryRaw }
--- test.ts
+++ test.ts
@@ -1,87 +1,87 @@
-import { goTry, goExpect, goTryRaw } from "./index";
+import { goExpect, goTry, goTryRaw } from './index'
 
-describe("go-go-try", () => {
+describe('go-go-try', () => {
 	it(`value returned by callback is used when callback doesn't throw`, async () => {
-		const fn = () => "value";
-		const [err1, value] = goTry(fn);
-		let value2;
+		const fn = () => 'value'
+		const [err1, value] = goTry(fn)
+		let value2
 		try {
-			value2 = await goExpect(fn);
+			value2 = await goExpect(fn)
 		} catch (err) {
 			// this won't throw
 		}
-		const [err2, value3] = goTryRaw(fn);
-		expect(value).toBe("value");
-		expect(value2).toBe("value");
-		expect(value3).toBe("value");
-		expect(err1).toBeUndefined();
-		expect(err2).toBeUndefined();
-	});
+		const [err2, value3] = goTryRaw(fn)
+		expect(value).toBe('value')
+		expect(value2).toBe('value')
+		expect(value3).toBe('value')
+		expect(err1).toBeUndefined()
+		expect(err2).toBeUndefined()
+	})
 
-	it("if callback throws, value should be undefined and err should contain the error message", async () => {
+	it('if callback throws, value should be undefined and err should contain the error message', async () => {
 		const fn = () => {
-			return JSON.parse("{/");
-		};
-		const [err1, value] = goTry(fn);
-		let value2;
-		let err2;
+			return JSON.parse('{/')
+		}
+		const [err1, value] = goTry(fn)
+		let value2
+		let err2
 		try {
-			value2 = await goExpect(fn);
+			value2 = await goExpect(fn)
 		} catch (err) {
 			if (err instanceof Error) {
-				err2 = err.message;
+				err2 = err.message
 			}
 		}
-		const [err3, value3] = goTryRaw(fn);
+		const [err3, value3] = goTryRaw(fn)
 
-		expect(value).toBeUndefined();
-		expect(value2).toBeUndefined();
-		expect(value3).toBeUndefined();
-		expect(typeof err1).toBe("string");
-		expect(typeof err2).toBe("string");
-		expect(typeof err3).toBe("object");
-		expect(typeof (err3 as { message: string }).message).toBe("string");
-	});
+		expect(value).toBeUndefined()
+		expect(value2).toBeUndefined()
+		expect(value3).toBeUndefined()
+		expect(typeof err1).toBe('string')
+		expect(typeof err2).toBe('string')
+		expect(typeof err3).toBe('object')
+		expect(typeof (err3 as { message: string }).message).toBe('string')
+	})
 
-	it("first parameter accepts promises and makes the function async", async () => {
-		const fn = Promise.resolve("value");
-		const [err1, value] = await goTry(fn);
-		let value2;
-		let err2;
+	it('first parameter accepts promises and makes the function async', async () => {
+		const fn = Promise.resolve('value')
+		const [err1, value] = await goTry(fn)
+		let value2
+		let err2
 		try {
-			value2 = await goExpect(fn);
+			value2 = await goExpect(fn)
 		} catch (err) {
-			err2 = err;
+			err2 = err
 		}
-		const [err3, value3] = await goTryRaw(fn);
-		expect(value).toBe("value");
-		expect(value2).toBe("value");
-		expect(value3).toBe("value");
-		expect(err1).toBeUndefined();
-		expect(err2).toBeUndefined();
-		expect(err3).toBeUndefined();
-	});
+		const [err3, value3] = await goTryRaw(fn)
+		expect(value).toBe('value')
+		expect(value2).toBe('value')
+		expect(value3).toBe('value')
+		expect(err1).toBeUndefined()
+		expect(err2).toBeUndefined()
+		expect(err3).toBeUndefined()
+	})
 
-	it("if async callback throws, value should be undefined and err should contain the error message", async () => {
-		const fn = Promise.reject(new Error("error"));
-		const [err, value] = await goTry(fn);
-		let value2;
-		let err2;
+	it('if async callback throws, value should be undefined and err should contain the error message', async () => {
+		const fn = Promise.reject(new Error('error'))
+		const [err, value] = await goTry(fn)
+		let value2
+		let err2
 		try {
-			value2 = await goExpect(fn, (e) => `custom ${e}`);
+			value2 = await goExpect(fn, (e) => `custom ${e}`)
 		} catch (err) {
 			if (err instanceof Error) {
-				err2 = err.message;
-				console.log(err);
+				err2 = err.message
+				console.log(err)
 			}
 		}
-		const [err3, value3] = await goTryRaw(fn);
-		expect(value).toBeUndefined();
-		expect(value2).toBeUndefined();
-		expect(value3).toBeUndefined();
-		expect(err).toBe("error");
-		expect(err2).toBe("custom error");
-		expect(typeof err3).toBe("object");
-		expect((err3 as { message: string }).message).toEqual("error");
-	});
-});
+		const [err3, value3] = await goTryRaw(fn)
+		expect(value).toBeUndefined()
+		expect(value2).toBeUndefined()
+		expect(value3).toBeUndefined()
+		expect(err).toBe('error')
+		expect(err2).toBe('custom error')
+		expect(typeof err3).toBe('object')
+		expect((err3 as { message: string }).message).toEqual('error')
+	})
+})
